859cb4ecf0a7458c47bb4ae71ce23f8ba5df3131
PosetMage <mail@posetmage.com>
2024-04-23 15:00:59 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git config --local user.name "PosetMage"
-git config --local user.email "PosetMage@users.noreply.github.com"
+git config --local user.email "mail@posetmage.com"
 git remote set-url origin git@POM:posetmage/Images.git
 
 git pull
